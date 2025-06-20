b7f78262b8226d6c51f94364199d1bdeceb99022
Itztiva <tiva@Itztiva.com>
2025-06-19 20:23:40 -0400

    compact

--- Neon/FortniteGame/FortGameState/Source/FortGameState.cpp
+++ Neon/FortniteGame/FortGameState/Source/FortGameState.cpp
@@ -3,17 +3,11 @@
 
 void AFortGameStateAthena::OnRep_CurrentPlaylistId()
 {
-    static auto OnRep_CurrentPlaylistId = (UFunction*)GUObjectArray.FindObject("/Script/FortniteGame.FortGameStateAthena.OnRep_CurrentPlaylistId");
-
-    if (OnRep_CurrentPlaylistId)
-        this->ProcessEvent(OnRep_CurrentPlaylistId, nullptr);
+    this->CallFunc<void>("FortGameStateAthena","OnRep_CurrentPlaylistId");
 }
 
 
 void AFortGameStateAthena::OnRep_CurrentPlaylistInfo()
 {
-    static auto OnRep_CurrentPlaylistInfo = (UFunction*)GUObjectArray.FindObject("/Script/FortniteGame.FortGameStateAthena.OnRep_CurrentPlaylistInfo");
-
-    if (OnRep_CurrentPlaylistInfo)
-        this->ProcessEvent(OnRep_CurrentPlaylistInfo, nullptr);
+    this->CallFunc<void>("FortGameStateAthena","OnRep_CurrentPlaylistInfo");
 }
