92336b850d209bec350de8c495ec2731606238c0
Itztiva <tiva@Itztiva.com>
2025-06-22 08:01:22 -0400

    finish ts later

--- Neon/FortniteGame/FortInventory/Header/FortInventory.h
+++ Neon/FortniteGame/FortInventory/Header/FortInventory.h
@@ -1,3 +1,16 @@
 #pragma once
 #include "pch.h"
 
+#include "FortniteGame/FortPlayerController/Header/FortPlayerController.h"
+
+
+struct FFortItemEntry : FFastArraySerializerItem
+{
+};
+
+class AFortInventory : public UObject
+{
+public:
+    void Update(AFortPlayerControllerAthena* PlayerController, UObject* Entry);
+    UObject* GiveItem(AFortPlayerControllerAthena* PlayerController, UObject* Def, int Count, int LoadedAmmo, int Level);
+};
---
+++ Neon/FortniteGame/FortInventory/Source/FortInventory.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "FortniteGame/FortInventory/Header/FortInventory.h"
+
+void AFortInventory::Update(AFortPlayerControllerAthena* PlayerController, UObject* Entry)
+{
+    if (!PlayerController) return;
+    auto WorldInventory = PlayerController->Get<UObject*>("FortPlayerController", "WorldInventory");
+    auto Inventory = WorldInventory->Get<UStruct*>("FortInventory", "Inventory");
+    WorldInventory->Set("FortInventory", "bRequiresLocalUpdate", true);
+    WorldInventory->CallFunc<void>("FortInventory", "HandleInventoryLocalUpdate");
+
+    if (Entry)
+    {
+        auto EntryPtr = reinterpret_cast<FFastArraySerializerItem*>(Entry);
+        FFastArraySerializerItem& EntryRef = *EntryPtr;
+        ((FFastArraySerializer*)Inventory)->MarkItemDirty(EntryRef);
+        ((FFastArraySerializer*)Inventory)->MarkArrayDirty();
+    } else
+    {
+        ((FFastArraySerializer*)Inventory)->MarkArrayDirty();
+    }
+}
+
+UObject* AFortInventory::GiveItem(AFortPlayerControllerAthena* PlayerController, UObject* Def, int Count, int LoadedAmmo, int Level)
+{
+    if (!PlayerController || !Def) return nullptr;
+}
