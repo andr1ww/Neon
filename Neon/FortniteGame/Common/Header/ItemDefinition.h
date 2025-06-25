05086f5f9c65e5637176662d9a5a6b6c442eb654
NotTacs <142945024+NotTacs@users.noreply.github.com>
2025-06-24 21:20:15 -0400

    Update ItemDefinition.h

--- Neon/FortniteGame/Common/Header/ItemDefinition.h
+++ Neon/FortniteGame/Common/Header/ItemDefinition.h
@@ -6,18 +6,7 @@ class UFortItemDefinition : public UObject
 public:
     UObject* CreateTemporaryItemInstanceBP(int32 Count, int32 Level)
     {
-        struct FortItemDefinition_CreateTemporaryItemInstanceBP final
-        {
-        public:
-            int32                                                 Count;                                             // 0x0000(0x0004)(Parm, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-            int32                                                 Level;                                             // 0x0004(0x0004)(Parm, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-            UObject*                               ReturnValue;                                       // 0x0008(0x0008)(Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-        };
 
-        FortItemDefinition_CreateTemporaryItemInstanceBP Params{};
-
-        Params.Count = Count;
-        Params.Level = Level;
     
         static SDK::UFunction* Func = nullptr;
 
@@ -30,6 +19,19 @@ public:
             UE_LOG(LogNeon, Fatal, "Failed to find function CreateTemporaryItemInstanceBP in FortItemDefinition");
             return nullptr;
         }
+
+        struct FortItemDefinition_CreateTemporaryItemInstanceBP final
+        {
+        public:
+            int32                                                 Count;                                             // 0x0000(0x0004)(Parm, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
+            int32                                                 Level;                                             // 0x0004(0x0004)(Parm, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
+            UObject* ReturnValue;                                       // 0x0008(0x0008)(Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
+        };
+
+        FortItemDefinition_CreateTemporaryItemInstanceBP Params{};
+
+        Params.Count = Count;
+        Params.Level = Level;
         
         auto Flgs = Func->FunctionFlags();
         Func->FunctionFlags() |= 0x400;
