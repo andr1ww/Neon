2b8c1fa97dce5c4a5c70d995c2380fc781f77a17
Heliato <anthonybetton91600@gmail.com>
2025-06-25 04:05:00 +0200

    Update ItemDefinition.h

--- Neon/FortniteGame/Common/Header/ItemDefinition.h
+++ Neon/FortniteGame/Common/Header/ItemDefinition.h
@@ -6,38 +6,63 @@ class UFortItemDefinition : public UObject
 public:
     class UFortWorldItem* CreateTemporaryItemInstanceBP(int32 Count, int32 Level)
     {
-        struct FortItemDefinition_CreateTemporaryItemInstanceBP final
-        {
-        public:
-            int32                                                 Count;                                             // 0x0000(0x0004)(Parm, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-            int32                                                 Level;                                             // 0x0004(0x0004)(Parm, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-            UFortWorldItem*                               ReturnValue;                                       // 0x0008(0x0008)(Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-        };
-
-        FortItemDefinition_CreateTemporaryItemInstanceBP Params{};
-
-        Params.Count = Count;
-        Params.Level = Level;
-    
-        static SDK::UFunction* Func = nullptr;
-
         SDK::FFunctionInfo Info = SDK::PropLibrary->GetFunctionByName("FortItemDefinition", "CreateTemporaryItemInstanceBP");
 
-        if (Func == nullptr)
-            Func = Info.Func;
-        if (!Func)
+        if (Info.Func)
         {
-            UE_LOG(LogNeon, Fatal, "Failed to find function CreateTemporaryItemInstanceBP in FortItemDefinition");
-            return nullptr;
+            size_t ParmsSize = 0x10;//Info.Func->GetParmsSize();
+            void* Parms = ::malloc(ParmsSize);
+
+            if (Parms)
+            {
+                memset(Parms, 0, ParmsSize);
+
+                *(int32*)(__int64(Parms) + 0x0) = Count;
+                *(int32*)(__int64(Parms) + 0x4) = Level;
+
+                ProcessEvent(Info.Func, Parms);
+
+                UFortWorldItem* ReturnValue = *(UFortWorldItem**)(__int64(Parms) + 0x8);
+                ::free(Parms);
+
+                return ReturnValue;
+            }
         }
-        
-        /*auto Flgs = Func->FunctionFlags();
-        Func->FunctionFlags() |= 0x400;*/
 
-        ProcessEvent(Func, &Params);
+        return nullptr;
 
-        //Func->FunctionFlags() = Flgs;
+        //struct FortItemDefinition_CreateTemporaryItemInstanceBP final
+        //{
+        //public:
+        //    int32                                                 Count;                                             // 0x0000(0x0004)(Parm, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
+        //    int32                                                 Level;                                             // 0x0004(0x0004)(Parm, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
+        //    UFortWorldItem*                               ReturnValue;                                       // 0x0008(0x0008)(Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
+        //};
 
-        return Params.ReturnValue;
+        //FortItemDefinition_CreateTemporaryItemInstanceBP Params{};
+
+        //Params.Count = Count;
+        //Params.Level = Level;
+    
+        //static SDK::UFunction* Func = nullptr;
+
+        //SDK::FFunctionInfo Info = SDK::PropLibrary->GetFunctionByName("FortItemDefinition", "CreateTemporaryItemInstanceBP");
+
+        //if (Func == nullptr)
+        //    Func = Info.Func;
+        //if (!Func)
+        //{
+        //    UE_LOG(LogNeon, Fatal, "Failed to find function CreateTemporaryItemInstanceBP in FortItemDefinition");
+        //    return nullptr;
+        //}
+        //
+        ///*auto Flgs = Func->FunctionFlags();
+        //Func->FunctionFlags() |= 0x400;*/
+
+        //ProcessEvent(Func, &Params);
+
+        ////Func->FunctionFlags() = Flgs;
+
+        //return Params.ReturnValue;
     }
 };
