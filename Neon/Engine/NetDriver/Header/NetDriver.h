aeeba578cdda92d28fca42926a8e9ea9acd3951c
Itztiva <tiva@Itztiva.com>
2025-07-28 14:29:30 -0400

    120.f -> 60.f

--- Neon/Engine/NetDriver/Header/NetDriver.h
+++ Neon/Engine/NetDriver/Header/NetDriver.h
@@ -318,7 +318,7 @@ public:
         return *reinterpret_cast<int32*>(__int64(this) + RepFrameOffset);
     }
     // Replication
-    static inline float GetMaxTickRate() { return 120.f; }
+    static inline float GetMaxTickRate() { return 60.f; }
     static int32 ServerReplicateActors(UNetDriver* NetDriver, float DeltaSeconds);
     static int32 ServerReplicateActors_PrepConnections(UNetDriver*, float DeltaSeconds);
     static void ServerReplicateActors_BuildConsiderList(UNetDriver* Driver, TArray<AActor*>& OutConsiderList, const float ServerTickTime);
