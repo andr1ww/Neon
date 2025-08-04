abb6e10fa84f8fc9c55849bacb48e2eba5168f9d
Itztiva <tiva@Itztiva.com>
2025-08-03 20:33:56 -0400

    Revert "Update FortGameMode.cpp"
    This reverts commit 152a617f4fe0f9de9bbd789507a00b3d3ea8a1cd.

--- Neon/FortniteGame/FortGameMode/Source/FortGameMode.cpp
+++ Neon/FortniteGame/FortGameMode/Source/FortGameMode.cpp
@@ -133,7 +133,7 @@ bool AFortGameModeAthena::ReadyToStartMatch(AFortGameModeAthena* GameMode, FFram
                 
                 AFortAIDirector* AIDirector = UGameplayStatics::SpawnActor<AFortAIDirector>({});
                 GameMode->SetAIDirector(AIDirector);
-                AIDirector->CallFunc<void>("FortAIDirector", "Activate");
+                AIDirector->Activate();
 
                 AFortAIGoalManager* AIGoalManager = UGameplayStatics::SpawnActor<AFortAIGoalManager>({});
                 GameMode->SetAIGoalManager(AIGoalManager);
